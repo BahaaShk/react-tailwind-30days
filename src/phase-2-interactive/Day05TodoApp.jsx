78058a3ebde11e857d3ee7138c651358acc748c0
BahaaShk <bahaa.shk@gmail.com>
2025-12-01 00:08:13 +0200

    finished the challenge of day 1

--- src/phase-2-interactive/Day05TodoApp.jsx
+++ src/phase-2-interactive/Day05TodoApp.jsx
@@ -25,8 +25,22 @@ const handleSubmit = (e) => {
   <h1 className=" text-2xl font-bold mb-4 text-center">Todo List</h1>
 
   <form onSubmit={handleSubmit} className="flex gap-2 mb-4">
-    <input type="text" value={input} onChange={(e) => setInput(e.target.value)} placeholder="Add a todo ..." />
+    <input type="text" value={input} onChange={(e) => setInput(e.target.value)} placeholder="Add a todo ..." className=" flex-1 border p-2 rounded-lg"/>
+
+    <button type="submit" className="bg-blue-600 text-white px-4 rounded-lg cursor-pointer">Add</button>
   </form>
+{todos.length === 0 && <p className=" text-center font-bold">Oops it's empty ! Add a todo to get started</p>}
+
+          <ul className="space-y-3">
+          {todos.map(todo => (
+            <li
+              key={todo.id}
+              className="p-2 bg-gray-200 rounded-lg shadow-lg flex justify-between items-center"
+            >
+              {todo.text}
+            </li>
+          ))}
+        </ul>
 </div>
     </div>
   )
